6a5cf325923fdc99de0e36d295bdb99baa4b5962
AnnaLinneaW <annalinneawiklund@gmail.com>
2023-02-14 13:21:43 +0100

    Skapat knappar i javascript

--- javascript/dayJava.js
+++ javascript/dayJava.js
@@ -1,30 +1,20 @@
 import { dataArray } from "./dataArray.js";
 export const runDailyElements = () => {
-
-let firstBtn = document.querySelector("firstBtn");
-let secondBtn = document.querySelector("secondBtn");
-let thirdBtn = document.querySelector("thirdBtn");
-let fourthBtn = document.querySelector("fourthBtn");
-let fifthBtn = document.querySelector("fifthBtn");
-let sixthBtn = document.querySelector("sixthBtn");
-let seventhBtn = document.querySelector("seventhBtn");
-let dayBtn= document.querySelectorAll("button");
-
-console.log(dataArray);
-
-const dailyName = dataArray[0].name;
-const dailyDate = dataArray[0].date;
-const dailyData = dataArray[0].daily.tempMax;
-
-
-console.log(dailyDate);
-console.log(dailyName);
-console.log(dailyData);
-
-let date = new Date();
-let currentDay = date.getDate() 
-let currentMonth =date.getMonth();
-let currentDate = currentDay + "/" + currentMonth;
-console.log(currentDate);
-
+    // Hämtar main-div
+    let mainDiv = document.querySelector(".main-daydiv");
+    // Loopar igenom dataArray och skapar knappar 
+    for (let i = 0; i < dataArray.length; i++) {
+        //skapar knappar
+      let button = document.createElement('button');
+      // Ger knappar inehåll från dataArray
+      button.innerHTML = `
+        <div class="dayBtn">
+          <h2>${dataArray[i].name} ${dataArray[i].date.day}/${dataArray[i].date.month}</h2>
+        </div>
+        <div>
+          <div>${dataArray[i].daily.weathercode}</div><div> ${dataArray[i].daily.tempMax} ${dataArray[i].daily.tempMin}</div>
+        </div>
+      `;
+      mainDiv.append(button);
+    }
 };
