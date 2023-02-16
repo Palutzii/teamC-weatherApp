bfa3d31c60750ee8ec4243b110dcbac1f781d26c
saganorth <saga.nord99@hotmail.se>
2023-02-16 11:29:21 +0100

    assdasd

--- javascript/heroelement.js
+++ javascript/heroelement.js
@@ -1,84 +1,82 @@
-import {currentWeatherObject} from "./javascript/getWeather.js";
-import { dataArray } from "../main.js";
-import { weatherIcons} from "./hourlyElement.js";
-import { runHourlyElement } from "./hourlyElement.js";
+
+import {currentWeatherObject} from "../main.js";
 
 
+ export const runHeroElement = () => {
+    const {
+      cloudBolt,
+      cloudRain,
+      cloudShowersHeavy,
+      cloudSunRain,
+      cloudSun,
+      cloud,
+      snowflake,
+      sun,
+      tornado,
+    } = weatherIcons;
 
- document.qetElementById("location").innerhtml= location;
- document.qetElementById("hero-day").innerhtml= name;
- document.qetElementById("day-date").innerhtml= date;
- document.qetElementById("hero-icon").innerhtml= weathercode;
- document.qetElementById("hero-temp").innerhtml= temp;
- document.qetElementById("feels").innerhtml= apparentTemp;
 
-class hero
- let hero = document.createElement('div');
-  hero.value = dataArray[0].name;
+     let heroinfo = document.querySelector(".hero-info");
+     let weathericon = "";
 
-
-export const runDailyElement = () => {
-  const { 
-    cloudBolt,
-    cloudRain,
-    cloudShowersHeavy,
-    cloudSunRain,
-    cloudSun,
-    cloud,
-    snowflake,
-    sun,
-    tornado,
-  } = weatherIcons;
-  // Hämtar main-div
-  let heroinfo = document.querySelector(".hero-info");
-  let infoDiv=document.querySelector(".infoDiv");
-  let timeInfo=document.querySelector(".timeInfo");
-  infoDiv.style.display="none";
-  timeInfo.style.display="none";
-  let weathericon="";
     // Loopar igenom dataArray och skapar knappar 
-    
+
     let hero = document.createElement('div');
-    hero.value = dataArray[0].name;
+    hero.value = currentWeatherObject[i].name;
         
-        if (dataArray[i].hourly.weathercode == 0) {
+        if (currentWeatherObject.weathercode == 0) {
           weathericon = sun;
-        } else if (dataArray[i].hourly.weathercode > 0 && dataArray[i].hourly.weathercode < 3) {
+        } else if (currentWeatherObject.weathercode > 0 && currentWeatherObject.weathercode < 3) {
           weathericon = cloudSun;
-        } else if (dataArray[i].hourly.weathercode == 3) {
+        } else if (currentWeatherObject.weathercode == 3) {
           weathericon = cloud;
-        } else if (dataArray[i].hourly.weathercode > 3 && dataArray[i].hourly.weathercode < 68) {
+        } else if (currentWeatherObject.weathercode > 3 && currentWeatherObject.weathercode < 68) {
           weathericon = cloudRain;
-        } else if (dataArray[i].hourly.weathercode > 79 && dataArray[i].hourly.weathercode < 83) {
+        } else if (currentWeatherObject.weathercode > 79 && currentWeatherObject.weathercode < 83) {
           weathericon = cloudShowersHeavy;
         } else if (
-          (dataArray[i].hourly.weathercode > 70 && dataArray[i].hourly.weathercode < 78) ||
-          (dataArray[i].hourly.weathercode > 84 && dataArray[i].hourly.weathercode < 87)
+          (currentWeatherObject.weathercode > 70 && currentWeatherObject.weathercode < 78) ||
+          (currentWeatherObject.weathercode > 84 && currentWeatherObject.weathercode < 87)
         ) {
           weathericon = snowflake;
         } else if (currentHour.weathercode > 94) {
           weathericon = cloudBolt;
         }
 
-      // Ger knappar inehåll från dataArray
+      // Ger div inehåll från dataArray
       hero.innerHTML = `
       <div class ="hero-location">
-      <p>${location}</p>
+      <h1 id="location">${currentWeatherObject.location}</h1>
       </div>
         <div class="hero-date">
-          <p>${dataArray[i].name}</p> <p>${parseInt(dataArray[i].date.day,10)}/${parseInt(dataArray[i].date.month,10)}</p>
-        </div>
+         <span> <p id="hero-day">${currentWeatherObject.name}</p> <p id="day-date">${parseInt(currentWeatherObject.date,10)}</p>
+       </span>
+         </div>
+         </div>
+          <div class="hero-current">
         <div class="hero-icon">
-          <p>${weathericon}</p> <div class="hero-temp"> <p>${dataArray[i].hourly.apparentTemp}</p>  <p>${dataArray[i].hourly.apperenttemp}</p>
+          <p id="hero-icon">${currentWeatherObject.weathericon}</p>
+           <div class="hero-temp">
+
+           <p id="hero-temp">${currentWeatherObject.temp}</p> 
+           <div class="hero-feels">
+           <p> känns som <p>
+            <p id="feels">${currentWeatherObject.apperentTemp}</p>
+        </div>
         </div>
       `;
 
-     >
+    heroinfo.append(hero); 
+console.log(hero)
 
-   
+//hero.addEventListener("click", (event) => {
+//runHourlyElement(event.target.value)
+//heroinfo.innerHTML = "";
 
-    heroinfo.append(div); 
-console.log(div)
+ // })
+}; 
+
+
 
 hero.addEventListener("click", (event) => {
 runHourlyElement(event.target.value)
--- main.js
+++ main.js
@@ -2,13 +2,40 @@
 import { myLocation } from "./javascript/myLocation.js";
 import { getWeather } from "./javascript/getWeather.js";
 
+import {currentWeatherObject} from "./javascript/getWeather.js"; // Object att använda i "hero"
+export { currentWeatherObject };
+import { dataArray } from "./javascript/getWeather.js";
+export { dataArray };
+import { runHourlyElement } from "./javascript/hourlyElement.js";
+import { runDailyElement } from "./javascript/dayElement.js";
+import { runHeroElement } from "./javascript/heroelement.js";
+async function main(){
+
+// hittar och skriver ut koordinater för enhetens plats
+
+
 let latitude, longitude;
 
 myLocation((position) => {
   const coordinates = position.coords;
   latitude = coordinates.latitude;
   longitude = coordinates.longitude;
+
   getWeather(latitude, longitude);
+
+
+
+  console.log(dataArray)
+  console.log(currentWeatherObject)
+
+
+  // runHourlyElement(1);
+
+ // runHourlyElement();
+ runHeroElement();
+
+ runDailyElement();
+
 });
 
 // tillfällig array
