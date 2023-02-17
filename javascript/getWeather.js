e825f42abefd39df9088332f2f0d7cf35712c053
Tobias Olausson <tobias.olausson@yh.nackademin.se>
2023-02-17 14:26:52 +0100

    använder alltid vår tidzon

--- javascript/getWeather.js
+++ javascript/getWeather.js
@@ -78,7 +78,8 @@ let createArray = () => {
 
 export async function getWeather(latitude, longitude) {
     weatherData = "";
-    const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&hourly=temperature_2m,weathercode,apparent_temperature,rain,windspeed_10m,winddirection_10m&windspeed_unit=ms&daily=temperature_2m_max,temperature_2m_min,weathercode,precipitation_sum&timezone=auto`
+    // const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&hourly=temperature_2m,weathercode,apparent_temperature,rain,windspeed_10m,winddirection_10m&windspeed_unit=ms&daily=temperature_2m_max,temperature_2m_min,weathercode,precipitation_sum&timezone=auto`
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&hourly=temperature_2m,weathercode,apparent_temperature,rain,windspeed_10m,winddirection_10m&windspeed_unit=ms&daily=temperature_2m_max,temperature_2m_min,weathercode,precipitation_sum&timezone=Europe%2FStockholm`
     const res = await fetch(url);
     const data = await res.json();
     weatherData = data;
